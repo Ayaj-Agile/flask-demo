69e103a07b54e8e5f45fbd3a389fd83282f06267
Ayaj-Agile <ayaj.ahamad@agilehealthtech.co.in>
2025-06-20 16:30:24 +0530

    yml file added

--- test/test_app.py
+++ test/test_app.py
@@ -1,7 +1,11 @@
-from flask_main import main
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from flask_main.main import app
 
 def test_home():
-    tester = main.test_client()
-    response = tester.get('/')
+    client = app.test_client()
+    response = client.get('/')
     assert response.status_code == 200
     assert b"Hello from GitHub Actions!" in response.data
